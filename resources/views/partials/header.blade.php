2178bf6bf648ab60d59e482d4a9f1e0d55a96c89
FedeAle1525 <fedeale.1525@gmail.com>
2023-04-05 16:25:27 +0200

    FEAT add elements in Header + STYLE

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,19 @@
 <header>
-  HEADER
+  <div class="container">
+    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+
+    <!--MENU ITEMS: 'Characters', 'Comics', 'Movies', 'Tv', 'Games', 'Collectibles', 'Videos', 'Fans', 'News', 'Shop' -->
+    <ul>
+      <li>Characters</li>
+      <li>Comics</li>
+      <li>Movies</li>
+      <li>Tv</li>
+      <li>Games</li>
+      <li>Collectibles</li>
+      <li>Videos</li>
+      <li>Fans</li>
+      <li>News</li>
+      <li>Shop</li>
+    </ul>
+  </div>
 </header>
